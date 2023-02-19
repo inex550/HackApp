6c4ab9cefebffed7f1cf0bcbb7292458329b1d73
inex550 <inex552@gmail.com>
2023-02-19 08:16:56 +0300

    fix imports

--- app/src/main/java/ru/faaen/hackapp/features/flow/TabFragment.kt
+++ app/src/main/java/ru/faaen/hackapp/features/flow/TabFragment.kt
@@ -18,7 +18,6 @@ import ru.faaen.hackapp.core.prefs.PreferenceStorage
 import ru.faaen.hackapp.core.ui.base.BaseFragment
 import ru.faaen.hackapp.core.ui.ext.withArgs
 
-import ru.faaen.hackapp.features.profile.ProfileFragment
 import javax.inject.Inject
 
 @AndroidEntryPoint
